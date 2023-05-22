084a471782bf7871abda2486f6c72470c7f0a735
Tung Ngo <tungntgch220075.fpt.edu.vn>
2023-05-22 14:22:52 +0700

    ok

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('contact');
 });
-
-123
 
 module.exports = router;

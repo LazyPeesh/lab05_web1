f62d26233d15b72fa9b0410ddb945a273e9f9ec6
Tung Ngo <tungntgch220075.fpt.edu.vn>
2023-05-22 15:02:23 +0700

    k

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function (req, res) {
   res.render('contact');
 });
 
+router.get('/test', (req, res) => {
+
+})
+
+router.post('/message', (req, res) => {
+  // var data = req.body;
+  // var firstname = data.firstname;
+  // var lastname = data.lastname;
+  res.render('message', {data : req.body});
+});
+
 module.exports = router;
